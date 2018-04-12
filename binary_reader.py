e9d0434b351303cd0cb8bd619009f05c302aa8d7
Nino Rasic <nrasic@meteomatics.com>
2018-04-12 11:48:33 +0200

    Add binary_read and rounding files

---
+++ binary_reader.py
@@ -0,0 +1,76 @@
+import struct
+
+
+class BinaryReaderException(Exception):
+    def __init__(self, message):
+        super(BinaryReaderException, self).__init__(message)
+
+
+class BinaryReader(object):
+
+    DATA_TYPES = {
+        "double": {"format_char": "d", "num_of_bytes": 8},
+        "float": {"format_char": "f", "num_of_bytes": 4},
+        "int": {"format_char": "i", "num_of_bytes": 4},
+        "char": {"format_char": "c", "num_of_bytes": 1},
+        "unsigned_long": {"format_char": "Q", "num_of_bytes": 8}
+    }
+
+    def __init__(self, binary_data):
+        self._binary_data = bytearray(bytes(binary_data))
+        self._pointer = 0
+
+    def __str__(self):
+        return str(self._binary_data)
+
+    def __len__(self):
+        return len(self._binary_data[self._pointer:])
+
+    def _read_data(self, data_type, n):
+        try:
+            num_of_bytes = self.DATA_TYPES[data_type]["num_of_bytes"]
+        except KeyError as e:
+            raise BinaryReaderException("Data type: '{}' is not valid. Valid types: [{}]".format(data_type,
+                                                                                           ", ".join(self.DATA_TYPES.keys())))
+
+        substring = self._binary_data[self._pointer:self._pointer+num_of_bytes*n]
+
+        if len(substring) != num_of_bytes*n:
+            raise BinaryReaderException("Not possible to read {} bytes, length of binary data is {} bytes".format(num_of_bytes*n,
+                                                                                                          len(self)))
+        self._pointer += num_of_bytes*n
+        return struct.unpack("<{}".format(self.DATA_TYPES[data_type]["format_char"]*n), substring)
+
+    def get(self, data_type, n=1):
+        try:
+            data_type = data_type.lower()
+        except AttributeError:
+            raise BinaryReaderException("Argument data_type has to be string")
+
+        data = self._read_data(data_type, n)
+        if n == 1:
+            return data[0]
+        else:
+            return data
+
+    def get_int(self, n=1):
+        return self.get("int", n)
+
+    def get_double(self, n=1):
+        return self.get("double", n)
+
+    def get_char(self, n=1):
+        return self.get("char", n)
+
+    def get_float(self, n=1):
+        return self.get("float", n)
+
+    def get_unsigned_long(self, n=1):
+        return self.get("unsigned_long", n)
+
+    def get_string(self, length):
+        if length > len(self):
+            raise BinaryReaderException("Not possible to read string with length {}, length of binary data is {}".format(length,
+                                                                                                                       len(self)))
+        data = self.get("char", length)
+        return "".join([c.decode("utf-8") for c in data])
---
+++ rounding.py
@@ -0,0 +1,108 @@
+decimal_places = dict([
+    ("cm",      1), # centimeters (snow height)
+    ("C",       1), # temperature
+    ("d",      -1), # days AND degrees...
+    ("dn",      5), # datenums
+    ("eur",     1), # euro
+    ("F",       1), # temperature
+    ("ft",      1), # feet (ceiling height)
+    ("gm3",     1), # absolute humidity
+    ("gdd",     1), # growind degree days
+    ("hdd",     1), # heating degree days
+    ("h",       1), # hours
+    ("hft",     1), # hectofeet
+    ("hPa",     1), # pressures
+    ("Pa",      1), # pressures
+    ("idx",    -1), # index
+    ("J",       1), # energy
+    ("K",       1), # temperature
+    ("kA",      1), # kilo Amperes for lightning strokes
+    ("kgkg",    5), # kg/kg for mixing ratio
+    ("kgm2",    2), # kg/m2 for snow melt & super cooled liquid water
+    ("kgm3",    1), # density
+    ("km",      1), # km (e.g. visibility)
+    ("kmh",     1), # wind speed
+    ("kn",      1), # wind speed
+    ("kW",      1), # power
+    ("Jkg",     3), # cape
+    ("m",      -1), # meters (elevation, roughness length)
+    ("ft",      1), # feet ceiling height
+    ("m3",      2), # cubic meters (discharge)
+    ("min",     1), # minutes
+    ("mm",      2), # millimeters (precipitation)
+    ("MW",      3), # power
+    ("ms",      1), # wind speed
+    ("octas",   0), # clouds
+    ("p",       1), # percent
+    ("Pa",      1), # pressures
+    ("Pas",     2), # vertical wind speed Pas / s
+    ("psu",     1), # psu (for water salinity)
+    ("s",       1), # seconds
+    ("ugm3",    3), # air pollution
+    ("ux",      1), # unix time stamps
+    ("W",       1), # power
+    ("Ws",      1), # energy
+    ("x",       0), # number
+    ("y",       0), # year
+    ]
+)
+
+
+def get_num_decimal_places(parameter):
+    try:
+        _, unit = parameter.split(":")
+        if "-" in unit:
+            unit = unit.split("-")[0]
+    except ValueError:
+        return 3
+
+    try:
+        number_of_decimals = decimal_places[unit]
+    except KeyError:
+        return 3
+
+    if number_of_decimals >= 0:
+        return number_of_decimals
+
+    if unit == "idx":
+        if parameter in ["phytophthora_negative:idx", "phytophthora_negative_prognose:idx" ,
+                         "fosberg_fire_weather_index:idx," "santa_ana_wind:idx" ,"north_atlantic_oscillation:idx"]:
+            return 1
+
+        elif parameter.find("icing_potential") == 0 or parameter.find("sld_potential") == 0:
+            return 2
+
+        elif parameter == "moon_phase:idx":
+            return 3
+
+        elif "_aod" in parameter:
+            return 3
+
+        if parameter.find("sat_") != -1:
+            if parameter.find("cloud_type", 4) != -1:
+                return 0
+            elif parameter.find("ndvi", 4) != -1:
+                return 2
+            else:
+                return 5
+
+        return 0
+    elif unit == "d":
+        if "lats" in parameter or "lons" in parameter:
+            return 5
+
+    elif unit == "m":
+        if parameter == "roughness_length:m":
+            return 3
+        else:
+            return 1
+
+
+def round_df(df):
+    for column in df.columns:
+        sig_num = get_num_decimal_places(column)
+        df[column] = df[column].round(sig_num)
+    return df
+
+
+
