f296c9b1e92cb74effa73f58a93cb22c6e0efd60
Markus Neumann <maekke87@gmail.com>
2020-05-01 09:51:32 +0200

    refactor and cleanup (#20)
    * refactor and cleanup
    
    * add newline
    
    Co-authored-by: Markus Neumann <mneumann@meteomatics.com>

--- meteomatics/rounding.py
+++ meteomatics/rounding.py
@@ -1,53 +1,50 @@
 # -*- coding: utf-8 -*-
 
-decimal_places = dict([
-    ("cm",      1), # centimeters (snow height)
-    ("C",       1), # temperature
-    ("d",      -1), # days AND degrees...
-    ("dn",      5), # datenums
-    ("eur",     1), # euro
-    ("F",       1), # temperature
-    ("ft",      1), # feet (ceiling height)
-    ("gm3",     1), # absolute humidity
-    ("gdd",     1), # growind degree days
-    ("hdd",     1), # heating degree days
-    ("h",       1), # hours
-    ("hft",     1), # hectofeet
-    ("hPa",     1), # pressures
-    ("Pa",      1), # pressures
-    ("idx",    -1), # index
-    ("J",       1), # energy
-    ("K",       1), # temperature
-    ("kA",      1), # kilo Amperes for lightning strokes
-    ("kgkg",    5), # kg/kg for mixing ratio
-    ("kgm2",    2), # kg/m2 for snow melt & super cooled liquid water
-    ("kgm3",    1), # density
-    ("km",      1), # km (e.g. visibility)
-    ("kmh",     1), # wind speed
-    ("kn",      1), # wind speed
-    ("kW",      1), # power
-    ("Jkg",     3), # cape
-    ("m",      -1), # meters (elevation, roughness length)
-    ("ft",      1), # feet ceiling height
-    ("m3",      2), # cubic meters (discharge)
-    ("min",     1), # minutes
-    ("mm",      2), # millimeters (precipitation)
-    ("MW",      3), # power
-    ("ms",      1), # wind speed
-    ("octas",   0), # clouds
-    ("p",       1), # percent
-    ("Pa",      1), # pressures
-    ("Pas",     2), # vertical wind speed Pas / s
-    ("psu",     1), # psu (for water salinity)
-    ("s",       1), # seconds
-    ("ugm3",    3), # air pollution
-    ("ux",      1), # unix time stamps
-    ("W",       1), # power
-    ("Ws",      1), # energy
-    ("x",       0), # number
-    ("y",       0), # year
-    ]
-)
+decimal_places = {
+    "cm":       1,  # centimeters (snow height)
+    "C":        1,  # temperature
+    "d":       -1,  # days AND degrees...
+    "dn":       5,  # datenums
+    "eur":      1,  # euro
+    "F":        1,  # temperature
+    "ft":       1,  # feet (ceiling height)
+    "gm3":      1,  # absolute humidity
+    "gdd":      1,  # growind degree days
+    "hdd":      1,  # heating degree days
+    "h":        1,  # hours
+    "hft":      1,  # hectofeet
+    "hPa":      1,  # pressures
+    "Pa":       1,  # pressures
+    "idx":     -1,  # index
+    "J":        1,  # energy
+    "K":        1,  # temperature
+    "kA":       1,  # kilo Amperes for lightning strokes
+    "kgkg":     5,  # kg/kg for mixing ratio
+    "kgm2":     2,  # kg/m2 for snow melt & super cooled liquid water
+    "kgm3":     1,  # density
+    "km":       1,  # km (e.g. visibility)
+    "kmh":      1,  # wind speed
+    "kn":       1,  # wind speed
+    "kW":       1,  # power
+    "Jkg":      3,  # cape
+    "m":       -1,  # meters (elevation, roughness length)
+    "m3":       2,  # cubic meters (discharge)
+    "min":      1,  # minutes
+    "mm":       2,  # millimeters (precipitation)
+    "MW":       3,  # power
+    "ms":       1,  # wind speed
+    "octas":    0,  # clouds
+    "p":        1,  # percent
+    "Pas":      2,  # vertical wind speed Pas / s
+    "psu":      1,  # psu (for water salinity)
+    "s":        1,  # seconds
+    "ugm3":     3,  # air pollution
+    "ux":       1,  # unix time stamps
+    "W":        1,  # power
+    "Ws":       1,  # energy
+    "x":        0,  # number
+    "y":        0,  # year
+}
 
 
 def get_num_decimal_places(parameter):
@@ -67,8 +64,8 @@ def get_num_decimal_places(parameter):
         return number_of_decimals
 
     if unit == "idx":
-        if parameter in ["phytophthora_negative:idx", "phytophthora_negative_prognose:idx" ,
-                         "fosberg_fire_weather_index:idx," "santa_ana_wind:idx" ,"north_atlantic_oscillation:idx"]:
+        if parameter in ["phytophthora_negative:idx", "phytophthora_negative_prognose:idx",
+                         "fosberg_fire_weather_index:idx," "santa_ana_wind:idx", "north_atlantic_oscillation:idx"]:
             return 1
 
         elif parameter.find("icing_potential") == 0 or parameter.find("sld_potential") == 0:
@@ -111,6 +108,3 @@ def round_df(df):
             sig_num = get_num_decimal_places(column)
             df[column] = df[column].round(sig_num)
     return df
-
-
-
