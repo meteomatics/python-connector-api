111865327f6759cfdff51d33ed11781ec02835b3
Arjan Keeman <akeeman@users.noreply.github.com>
2020-07-15 08:03:18 +0200

    Update logger.py

--- meteomatics/logger.py
+++ meteomatics/logger.py
@@ -4,6 +4,8 @@ import sys
 
 def create_log_handler(fmt='%(asctime)s| %(levelname)s |%(message)s'):
     formatter = logging.Formatter(fmt)
+    
     handler = logging.StreamHandler(stream=sys.stderr)
     handler.setFormatter(formatter)
-    logging.root.addHandler(handler)
+    
+    logging.getLogger("meteomatics").addHandler(handler)
