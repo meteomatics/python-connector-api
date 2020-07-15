ec9c263eab3f6ee594945a3bde7544ef6b190e8a
Arjan Keeman <akeeman@users.noreply.github.com>
2020-07-15 07:47:31 +0200

    Ship an actual logger
    Now people only have to call the `create_log_handler` function to have the same output as before.

---
+++ meteomatics/logger.py
@@ -0,0 +1,9 @@
+import logging
+import sys
+
+
+def create_log_handler(fmt='%(asctime)s| %(levelname)s |%(message)s'):
+    formatter = logging.Formatter(fmt)
+    handler = logging.StreamHandler(stream=sys.stderr)
+    handler.setFormatter(formatter)
+    logging.root.addHandler(handler)
