8899082d2039c4bbcb3baa3e3a46c6711154a402
Arjan Keeman <akeeman@users.noreply.github.com>
2020-09-25 10:05:21 +0200

    be able to replicate old behaviour exactly

--- meteomatics/logger.py
+++ meteomatics/logger.py
@@ -2,10 +2,13 @@ import logging
 import sys
 
 
-def create_log_handler(fmt='%(asctime)s| %(levelname)s |%(message)s'):
+def create_log_handler(
+    fmt='%(asctime)s| %(levelname)s |%(message)s',
+    stream=sys.stderr,
+):
     formatter = logging.Formatter(fmt)
     
-    handler = logging.StreamHandler(stream=sys.stderr)
+    handler = logging.StreamHandler(stream=stream)
     handler.setFormatter(formatter)
     
     logging.getLogger("meteomatics").addHandler(handler)
