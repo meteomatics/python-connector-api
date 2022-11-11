0bb082aca80e4e509e58d457fdee2bf4758eec23
gcostagliola <gcostagliola@meteomatics.com>
2022-11-11 11:01:44 +0100

    Fixed parse_url_for_post_data()

--- meteomatics/parsing_util.py
+++ meteomatics/parsing_util.py
@@ -332,11 +332,11 @@ def parse_time_series_params(model=None, ens_select=None, cluster_select=None, i
 
 def parse_url_for_post_data(url):
     """Split the url between url and data if needed"""
-    url_splitted = url.split("/", 4)
+    url_splitted = url.split("/", 3)
     data = None
-    if len(url_splitted) > 4:
-        url = "/".join(url_splitted[0:4])
-        data = url_splitted[4]
+    if len(url_splitted) > 3:
+        url = "/".join(url_splitted[0:3])
+        data = url_splitted[3]
     return url, data
 
 
