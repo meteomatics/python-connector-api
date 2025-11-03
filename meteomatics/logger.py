10d21c9c17f0a8f95e33ea35d6d0b9af3e8b30ce
bbuman-meteomatics <bbuman@meteomatics.com>
2025-11-03 16:27:09 +0100

    Fix typo.

--- meteomatics/logger.py
+++ meteomatics/logger.py
@@ -15,7 +15,7 @@ def create_log_handler(fmt='%(asctime)s| %(levelname)s |%(message)s', stream=sys
 
 
 def set_log_level(level):
-    """Use this function change the internal log level of the Meteomatics API Python connector."""
+    """Use this function to change the internal log level of the Meteomatics API Python connector."""
     if logging.getLogger(LOGGERNAME) is None:
         create_log_handler()
 
