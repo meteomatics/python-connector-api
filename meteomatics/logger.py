e92660d7b9e889cc384cf0a830c330638c7e9b6d
bbuman-meteomatics <bbuman@meteomatics.com>
2025-11-04 14:01:06 +0100

    Update docstring for log level.

--- meteomatics/logger.py
+++ meteomatics/logger.py
@@ -15,7 +15,13 @@ def create_log_handler(fmt='%(asctime)s| %(levelname)s |%(message)s', stream=sys
 
 
 def set_log_level(level):
-    """Use this function to change the internal log level of the Meteomatics API Python connector."""
+    """Use this function to change the internal log level of the Meteomatics API Python connector.
+
+    Note
+    ----
+    The log level is propagated to the Python logging library and so it's just an integer. Look at the aliases defined
+    in the library (https://docs.python.org/3/library/logging.html#logging-levels) for convenience.
+    """
     logger = logging.getLogger(LOGGERNAME)
 
     if not logger.handlers:
